1ac18348bd357c79211308520a6d8dfbbfedb48d
brent-soan <brent_soan@dlsu.edu.ph>
2024-03-14 22:33:54 +0800

    Added JSON files, added phase 2 files, and modified login.html

---
+++ Phase 2/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+var hbs = require("hbs");
+
+app.set("view engine", "hbs"); // Set view engine to handlebars
+app.use(express.static("public")) // Makes files in public accessible
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
+
